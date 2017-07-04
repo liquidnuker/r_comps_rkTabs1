cbe70d169b1e1e3a4894976c8fd42ae893febf35
administrator <liquidnuker@users.noreply.github.com>
2017-07-04 10:45:31 -0700

    initial commit. tabitem injected ok

---
+++ dist/index.bundle.js
@@ -0,0 +1,156 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+function TabItem1(props) {
+  return React.createElement(
+    'div',
+    null,
+    'TabItem1'
+  );
+}
+
+var Tabs = function (_React$Component) {
+  _inherits(Tabs, _React$Component);
+
+  function Tabs(props) {
+    _classCallCheck(this, Tabs);
+
+    var _this = _possibleConstructorReturn(this, (Tabs.__proto__ || Object.getPrototypeOf(Tabs)).call(this, props));
+
+    _this.state = {
+      items: [{
+        tabName: 'tab1'
+      }, {
+        tabName: 'tab2'
+      }]
+    };
+
+    // binders
+    return _this;
+  }
+
+  // lifecycle hooks
+
+
+  _createClass(Tabs, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      console.log("componentDidMount");
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {}
+    // abort
+
+
+    // methods  
+
+
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'nav',
+          { className: 'tabs1-01' },
+          this.state.items.map(function (i) {
+            return React.createElement(
+              'div',
+              { className: 'tabs', key: i.tabName },
+              i.tabName
+            );
+          })
+        ),
+        React.createElement(TabItem1, null)
+      );
+    }
+  }]);
+
+  return Tabs;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Tabs, null), document.getElementById('root'));
+
+/***/ })
+/******/ ]);
---
+++ src/index.jsx
@@ -0,0 +1,60 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+function TabItem1(props) {
+  return (
+    <div>TabItem1</div>
+  );
+}
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          tabName: 'tab1'
+        },
+        {
+          tabName: 'tab2'
+        },
+      ]
+    };
+
+    // binders
+  }
+
+  // lifecycle hooks
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  
+  componentWillUnmount() {
+    // abort
+  }
+
+  // methods  
+  render() {
+    return (
+      <div>
+
+      <nav className="tabs1-01">
+      {this.state.items.map((i) =>
+        <div className="tabs" key={i.tabName}>
+        {i.tabName}
+        </div>
+        )}
+      </nav>
+      <TabItem1 />
+
+      </div> 
+      );
+    }  
+}
+
+
+
+
+ReactDOM.render(
+  <Tabs />,
+  document.getElementById('root')
+);
